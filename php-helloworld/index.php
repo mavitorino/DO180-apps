75e0a850de5aa5cfbb6acbbde777721b79690873
Student User <student@workstation.lab.example.com>
2022-09-15 10:10:02 -0400

    new commit

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A Change is comming!\n";
-print " Tomorow we will have news\n"
-print " Today we will have news\n"
+print "Interface do projeto novo em desenvolvimento DEVOPS team "\n";
 ?>
