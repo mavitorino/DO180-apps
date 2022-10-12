158da0ec866517579d89eac4e3814e870a993a1a
Student User <student@workstation.lab.example.com>
2022-10-12 19:34:45 -0400

    01

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,2 @@
-<?php
-print "Hello, World! php version is 12-1--2022 Aviso" . PHP_VERSION . "\n";
-print "A change is in the air !"\n";
+iorljp-helloworld-iorljp-s2i.apps.na46a.prod.ole.redhat.comprint "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
